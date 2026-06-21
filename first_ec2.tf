93e13a7a8583c6f0f52afb938aeabd290272e109
Hema16032000 <hemalathaj.2000@gmail.com>
2026-06-21 20:42:12 +0530

    changed first ec2.tf file

--- first_ec2.tf
+++ first_ec2.tf
@@ -1,6 +1,13 @@
 provider "aws" {
   region     = "us-east-1"
+  access_key = "Enter secret key"
+  secret_key = "Enter secret key"
 }
-resource "aws_iam_user" "demouser" {
-  name = "hemauser"
+resource "aws_instance" "testec2" {
+    ami = "ami-09639480113b0df96"
+    instance_type = "t3.micro"
+
+     tags = {
+       Name = "Terraform-Server"
+  }
 }
